791279913eb3330aef741e4907856d02abd76a1a
khiraling <khiraling.jeur@outlook.com>
2016-09-30 16:29:05 -0700

    Add files via upload

---
+++ Global.asax.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using NSKData.DataAccessAPI.App_Start;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace NSKData.DataAccessAPI
+{
+    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+    // visit http://go.microsoft.com/?LinkId=9394801
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {        
+            AreaRegistration.RegisterAllAreas();
+            //WebApiConfig.Register(GlobalConfiguration.Configuration);
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            WebApiExceptionConfig.RegisterExceptions(GlobalConfiguration.Configuration);
+        }       
+    }
+}
---
+++ StartUp.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Infrastructure;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Http;
+
+namespace DataAccessAPI
+{
+    public class StartUp
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var oauthProvider = new OAuthAuthorizationServerProvider
+            {
+                OnGrantResourceOwnerCredentials = async context =>
+                {
+                    if (context.UserName == "rranjan" && context.Password == "password@123")
+                    {
+                        var claimsIdentity = new ClaimsIdentity(context.Options.AuthenticationType);
+                        claimsIdentity.AddClaim(new Claim("user", context.UserName));
+                        context.Validated(claimsIdentity);
+                        return;
+                    }
+                    context.Rejected();
+                },
+                OnValidateClientAuthentication = async context =>
+                {
+                    string clientId;
+                    string clientSecret;
+                    if (context.TryGetBasicCredentials(out clientId, out clientSecret))
+                    {
+                        if (clientId == "rajeev" && clientSecret == "secretKey")
+                        {
+                            context.Validated();
+                        }
+                    }
+                }
+            };
+            var oauthOptions = new OAuthAuthorizationServerOptions
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/accesstoken"),
+                Provider = oauthProvider,
+                AuthorizationCodeExpireTimeSpan = TimeSpan.FromMinutes(1),
+                AccessTokenExpireTimeSpan = TimeSpan.FromMinutes(3),
+                SystemClock = new SystemClock()
+            };
+            app.UseOAuthAuthorizationServer(oauthOptions);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+
+            var config = new HttpConfiguration();
+            config.MapHttpAttributeRoutes();
+            app.UseWebApi(config);
+        }
+    }
+}
